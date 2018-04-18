18ef42bcf6f2c2af089694c383c045cdad6b16b3
Sebastian Urbath <Sebastian.urbath@itggot.se>
2018-04-18 13:52:33 +0200

    added smallest of three

---
+++ dev/min_of_three.rb
@@ -0,0 +1,14 @@
+def min_of_two(num1, num2, num3)
+    a = num1
+    b = num2
+    c = num3
+    smallest = b
+    if a < b 
+        smallest = a
+    end
+    if c < smallest
+        smallest = c
+    end
+    p smallest
+    return smallest
+end
