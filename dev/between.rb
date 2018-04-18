21e72d6c36dd7a331a04e6b0d827b73cac583c7d
Sebastian Urbath <Sebastian.urbath@itggot.se>
2018-04-18 13:40:01 +0200

    added between

---
+++ dev/between.rb
@@ -0,0 +1,11 @@
+def between(number_1, number_2, number_3)
+    a = number_1
+    b = number_2
+    c = number_3
+    output = false
+    if b < a && a < c
+        output = true
+    end
+    p output
+    return output
+end
