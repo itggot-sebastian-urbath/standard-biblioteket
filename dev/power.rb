cdde82bf27816c633369b6145ba7c9d04456ad08
Sebastian Urbath <Sebastian.urbath@itggot.se>
2018-04-25 13:50:38 +0200

    Added Färdig power

--- dev/power.rb
+++ dev/power.rb
@@ -2,6 +2,10 @@ def power(num1,num2)
     a = num1
     b = num2
     i = 0
-    output = 0
+    output = 1
     while i < b
-        a 
+        output = output * a
+        i = i + 1
+    end
+    return output 
+end
