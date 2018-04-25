fb8faba94a13b9c02fc05cf77fa7e47a8ade7b26
Sebastian Urbath <Sebastian.urbath@itggot.se>
2018-04-25 14:25:18 +0200

    redigerad concat added

--- dev/concat.rb
+++ dev/concat.rb
@@ -2,9 +2,6 @@ def concat(arr1,arr2)
     a = arr1
     b = arr2
     i = 0
-    while i < arr2.length
-        a << b[i]
-        i = i + 1
-    end
-    return arr1
+    output = arr1 + arr2
+    return output
 end
