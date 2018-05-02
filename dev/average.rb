4200028ecdd637b1e616b0b7e13371d35fa9eeb2
Sebastian Urbath <Sebastian.urbath@itggot.se>
2018-05-02 14:21:25 +0200

    korrigerad average

--- dev/average.rb
+++ dev/average.rb
@@ -1,9 +1,11 @@
-def average(arr)
+def avarage(arr)
     output = 0
     i = 0
     while i < arr.length
         output = output + arr[i]
         i = i + 1
     end
-    return output / arr.length
+    return output.to_f / arr.length
 end
+
+p avarage([1337,1337,1337])
---
+++ dev/is_empty.rb
@@ -0,0 +1,9 @@
+def is_empty(string)
+    output = true
+    if string.length > 0
+        output = false
+    end 
+    return output
+end
+
+p is_empty("hej hopp")
